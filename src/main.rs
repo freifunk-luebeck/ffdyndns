e14f95c23ed3da3079961db022e703768b719ce0
Paul Maruhn <paulmaruhn@posteo.de>
2021-12-30 04:12:20 +0100

    fix #15

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,17 @@ use crate::db::Database;
 use lazy_static::lazy_static;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace};
-use rocket;
+use rocket::launch;
 use std::fs;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path;
 use std::process::exit;
 use toml;
 use pretty_env_logger;
+use clap::{self, App, Arg, ArgMatches};
+use std::time;
+use std::process;
+use tokio;
 
 const CONFIG_DIRS: &[&str] = &[
 	"./ffdyndns.toml",
@@ -78,11 +82,76 @@ macro_rules! sha256 {
 }
 
 
-#[rocket::main]
-async fn main() {
+fn main() {
 	pretty_env_logger::init();
 	// println!("{:?}", CONFIG.domain);
 
+	let app = clap::App::new(env!("CARGO_PKG_NAME"))
+		.subcommand(App::new("server"))
+		.subcommand(App::new("genzones")
+			.arg(Arg::with_name("out")
+				.required(true)
+				.long("out")
+				.takes_value(true)
+				.default_value("./"))
+			.arg(Arg::with_name("rname")
+				.takes_value(true)
+				.long("rname")
+				.required(true))
+			.arg(Arg::with_name("ns")
+				.multiple(true)
+				.min_values(1)
+				.takes_value(true)
+				.long("ns")
+				.required(true)));
+
+
+	match app.get_matches().subcommand() {
+		("server", Some(args)) => cmd_server(args),
+		("genzones", Some(args)) => cmd_genzones(args),
+		_ => {
+			error!("try --help");
+			process::exit(1)
+		}
+	}
+
+	process::exit(0);
+}
+
+pub fn cmd_genzones(args: &ArgMatches<'_>) {
+	let outdir = args.value_of("out").unwrap();
+	let rname = args.value_of("rname").unwrap();
+	let serial = time::SystemTime::now().duration_since(time::UNIX_EPOCH)
+		.unwrap()
+		.as_secs();
+
+	for domain in &CONFIG.domain {
+		let mut outstr = vec![];
+		outstr.push(format!("$ORIGIN ."));
+		outstr.push(format!("$TTL 30 ; 30 seconds"));
+		outstr.push(format!("{} IN SOA  {} {} (", domain.name, args.values_of("ns").unwrap().next().unwrap() , rname));
+		outstr.push(format!("	{} ; serial", serial));
+		outstr.push(format!("	3600 ; refresh (1 hour)"));
+		outstr.push(format!("	600 ; retry (10 minutes)"));
+		outstr.push(format!("	2600 ; expire (43 minutes 20 seconds)"));
+		outstr.push(format!("	30 ; minimum (30 seconds)"));
+		outstr.push(format!(")"));
+		for ns in args.values_of("ns").unwrap() {
+			outstr.push(format!("NS {}", ns));
+		}
+		outstr.push(format!(""));
+
+		let out = outstr.join("\n");
+
+		let mut outfile = fs::File::create(path::Path::new(&format!("{}/{}.zone", outdir, &domain.name))).unwrap();
+		outfile.write_all(out.as_bytes()).unwrap();
+	}
+}
+
+
+pub fn cmd_server(_: &ArgMatches<'_>) {
+	let rt = tokio::runtime::Runtime::new().unwrap();
 	let db = db::Database::new(CONFIG.database.clone().into());
-	web::start_web(db).await;
+
+	rt.block_on(web::start_web(db));
 }
